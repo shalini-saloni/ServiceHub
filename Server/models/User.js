d068c34f4efafad7909cf4f059b4a78f43bdd234
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-02-03 20:15:50 +0530

    Iniated backend

---
+++ Server/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
