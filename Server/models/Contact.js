7b176f2481840208f8d84e4f8501aee15aad219f
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-02-03 20:17:46 +0530

    Added Contact model

---
+++ Server/models/Contact.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  service: { type: String, required: true },
+  message: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
